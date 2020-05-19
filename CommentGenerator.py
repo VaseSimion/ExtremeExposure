c5d3c79b3d45cfe0638bb4abfa7b7d63b34757b0
VaseSimion <42439265+VaseSimion@users.noreply.github.com>
2020-05-19 18:58:58 +0200

    Added comment generator file and the one for image manipulation

---
+++ CommentGenerator.py
@@ -0,0 +1,40 @@
+import random
+
+def getauthor(url):
+    if 'by' in url.split('-'):
+        authorlist=url.split('-')[url.split('-').index('by')+1:]
+        print(authorlist)
+        temporaryauthorlist=[]
+        for name in authorlist:
+            if "?" not in name:
+                temporaryauthorlist.append(name)
+            else:
+                temporaryauthorlist += name.split('-')[0]
+
+        author = " ".join(temporaryauthorlist)
+    else:
+        author=""
+    print(author)
+    return author
+
+
+def getcomment(topic, author):
+    first_part_comment = ["Awesome ", "Amazing ", "Excellent ", "Stunning ", "Cool ", "Lovely ", "Wonderful ",
+                          "Superb ", "Spectacular ", "Great ", "Fantastic ", "Nice ", "Impressive ", "Fabulous ",
+                          "Splendid ", "Captivating "]
+
+    special_first_part = ["What a ", "Love this ", "Daaaamn, ", "", "", ""]
+
+    second_part_comment = ["image", "photo", "shot", "work", "picture", "frame", "snapshot"]
+
+    comment = random.choice(special_first_part)
+    if comment == "":
+        comment += random.choice(first_part_comment)
+        comment += random.choice(second_part_comment)
+    else:
+        comment += random.choice(first_part_comment).lower()
+        comment += random.choice(second_part_comment)
+    if author == "":
+        return comment
+    else:
+        comment += (", " + author + "!")
---
+++ ImageManipulation.py
@@ -0,0 +1,22 @@
+import cv2
+import pyautogui
+
+
+def get_resized_screenshot():
+    desired_size = 256
+    pyautogui.screenshot("screenshot.png")
+    image = cv2.imread("screenshot.png")
+    image = image[130:880, 220:1720]
+
+    old_size = image.shape[:2]  # old_size is in (height, width) format
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+    # new_size should be in (width, height) format
+    image = cv2.resize(image, (new_size[1], new_size[0]))
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    color = [0, 0, 0]
+    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+    return image
